44cde528d48579e3155fe9b58d7fae1ccb1ba71f
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-09-20 19:47:43 +0300

    vlang: fix missing files

--- packages/vlang/build.sh
+++ packages/vlang/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Simple, fast, safe, compiled language for developing mai
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=0.2.2
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/vlang/v/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9152eec96d2eeb575782cf138cb837f315e48c173878857441d98ba679e3a9bf
 TERMUX_PKG_DEPENDS="clang"
@@ -13,6 +14,6 @@ termux_step_make_install() {
 	install -Dm700 v "$TERMUX_PREFIX"/libexec/vlang/v
 	ln -sfr "$TERMUX_PREFIX"/libexec/vlang/v "$TERMUX_PREFIX"/bin/v
 	rm -rf "$TERMUX_PREFIX"/libexec/vlang/vlib
-	cp -a vlib "$TERMUX_PREFIX"/libexec/vlang/
+	cp -a cmd vlib "$TERMUX_PREFIX"/libexec/vlang/
 }
 
