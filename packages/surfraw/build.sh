b84c8186996faadd7bbdf18ad087f807b33ddb78
Leonid Pliushch <leonid.pliushch@gmail.com>
2021-10-24 19:23:40 +0000

    surfraw: fix source url

--- packages/surfraw/build.sh
+++ packages/surfraw/build.sh
@@ -4,8 +4,8 @@ TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=2.3.0
 TERMUX_PKG_REVISION=10
-TERMUX_PKG_SRCURL=https://gitlab.com/surfraw/Surfraw/repository/surfraw-$TERMUX_PKG_VERSION/archive.tar.bz2
-TERMUX_PKG_SHA256=d63c19c382b0e888e442f4106ee6babb5071ce076ab7834028db070814c42556
+TERMUX_PKG_SRCURL=https://gitlab.com/surfraw/Surfraw/-/archive/surfraw-${TERMUX_PKG_VERSION}/Surfraw-surfraw-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=b5e2b451a822ed437b165a5c81d8448570ee590db902fcd8174d6c51fcc4a16d
 TERMUX_PKG_DEPENDS="lynx, perl"
 TERMUX_PKG_BUILD_IN_SRC=true
 
